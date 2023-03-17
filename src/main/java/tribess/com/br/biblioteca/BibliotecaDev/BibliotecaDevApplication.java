134d25295f35fbd371da6185053c93ec87c1713e
Ericsson Tribess <ericktribess@gmail.com>
2023-03-17 18:58:13 -0300

    first commit

---
+++ src/main/java/tribess/com/br/biblioteca/BibliotecaDev/BibliotecaDevApplication.java
@@ -0,0 +1,13 @@
+package tribess.com.br.biblioteca.BibliotecaDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tribess/com/br/biblioteca/BibliotecaDev/BibliotecaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package tribess.com.br.biblioteca.BibliotecaDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
